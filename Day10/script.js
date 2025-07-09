54b7c5d5f12085a15cd2e3adc3509821d6e45768
Sugu4 <sulo4690@hotmail.com>
2025-07-09 10:53:36 +0200

    script.js Day10

---
+++ Day10/script.js
@@ -0,0 +1,30 @@
+let count = 0;
+
+function updateAnzeige() {
+  const anzeige = document.getElementById("anzeige");
+  anzeige.innerText = count;
+
+  // Farbe anpassen
+  if (count > 0) {
+    anzeige.className = "positiv";
+  } else if (count < 0) {
+    anzeige.className = "negativ";
+  } else {
+    anzeige.className = "";
+  }
+}
+
+function anzahlErhöhen() {
+  count++;
+  updateAnzeige();
+}
+
+function anzahlVerringern() {
+  count--;
+  updateAnzeige();
+}
+
+function zurücksetzen() {
+  count = 0;
+  updateAnzeige();
+}

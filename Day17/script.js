9f4b44af67b37068f21a886106cd5ae9c9b3a300
Sugu4 <sulo4690@hotmail.com>
2025-07-22 10:41:20 +0200

    script.js

---
+++ Day17/script.js
@@ -0,0 +1,16 @@
+const textfeld = document.getElementById("textfeld");
+const zaehler = document.getElementById("zaehler");
+const max = 160;
+
+textfeld.addEventListener("input", () => {
+  const laenge = textfeld.value.length;
+  zaehler.textContent = `${laenge} / ${max} Zeichen`;
+
+  if (laenge > 140) {
+    zaehler.className = "gefahr";
+  } else if (laenge > 100) {
+    zaehler.className = "warnung";
+  } else {
+    zaehler.className = "";
+  }
+});
